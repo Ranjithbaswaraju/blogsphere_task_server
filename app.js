10a750a289465fe4e24b2eb460fa26bbbf4248d5
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-12-23 21:15:05 +0530

    corsadded

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ app.use(express.urlencoded())
 connectDB()
 
 
-app.cors({
-    origin:["http://localhost:5173"]
-})
+app.use(
+    cors({
+        origin: ["http://localhost:5173"]
+    })
+)
 
 app.use("/auth",authRoutes)
 app.use("/post",postRoutes)
