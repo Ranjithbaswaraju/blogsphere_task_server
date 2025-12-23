be3d5b2034304a74f387cee3b2df189048d84ac6
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-12-23 21:43:20 +0530

    added

--- app.js
+++ app.js
@@ -14,14 +14,11 @@ app.use(express.urlencoded())
 connectDB()
 
 
-app.use(
-    cors({
-        origin: ["http://localhost:5173/"],
-         credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-    })
-)
+// app.use(
+//     cors({
+//         origin: "http://localhost:5173/"
+//     })
+// )
 
 app.use("/auth",authRoutes)
 app.use("/post",postRoutes)
