6d016bcc4f1c7f391a3e1085f1f6bd5cc601c12f
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-12-23 21:38:48 +0530

    updated

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ connectDB()
 
 app.use(
     cors({
-        origin: ["http://localhost:5173/"]
+        origin: ["http://localhost:5173/"],
+         credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
     })
 )
 
