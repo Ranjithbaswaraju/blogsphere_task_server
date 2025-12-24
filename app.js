96717c7e812aa3076a36c51660d2150fda0ef28c
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-12-24 14:48:27 +0530

    cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ connectDB()
 
 app.use(
     cors({
-        origin: ["blogsphere-task-client.vercel.app","http://localhost:5173"]
+        origin: ["ttps://blogsphere-task-client.vercel.app","http://localhost:5173"]
     })
 )
 
